82e4138a18f52bd4984a86754a27b26b26999994
Hank Wang <drapho@gmail.com>
2014-01-27 07:51:58 +0800

    Generate html by jade. fixed #1

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,40 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        jade: {
+            dev: {
+                options: {
+                    pretty: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    dest: './',
+                    src: '*.jade',
+                    ext: '.html'
+                }]
+            },
+            dist: {
+                options: {
+                    pretty: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    dest: 'dist/',
+                    src: '*.jade',
+                    ext: '.html'
+                }]
+            }
+        }
     });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-contrib-jade');
+
+    // Default task(s)
+    grunt.registerTask('build', ['jade:dist']);
+
+    // default task (with no arguments)
+    // grunt.registerTask('default', ['build']);
 };
